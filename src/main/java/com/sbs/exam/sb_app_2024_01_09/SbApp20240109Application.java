e1be82c954e9686ca559ba9a58ead58c918bc001
BobKim-H <devmann102@gmail.com>
2024-01-09 23:44:44 +0900

    BK_setting

---
+++ src/main/java/com/sbs/exam/sb_app_2024_01_09/SbApp20240109Application.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2024_01_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp20240109Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp20240109Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/exam/sb_app_2024_01_09/SbApp20240109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sb_app_2024_01_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp20240109ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
