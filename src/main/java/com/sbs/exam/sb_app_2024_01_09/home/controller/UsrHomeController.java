f97d11ef3e5db2070c203a0fed702287c4edb9c8
BobKim-H <devmann102@gmail.com>
2024-01-13 15:04:06 +0900

    count 조절 액션

--- src/main/java/com/sbs/exam/sb_app_2024_01_09/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2024_01_09/home/controller/UsrHomeController.java
@@ -2,42 +2,26 @@ package com.sbs.exam.sb_app_2024_01_09.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-  private int count =0;
+  int count;
   public UsrHomeController() {
-    count = 0;
-  }
-  @RequestMapping("/usr/home/main")
-  @ResponseBody
-  public String showMain() {
-    return "안녕하세요";
+    count = -1;
   }
 
-  @RequestMapping("/usr/home/main2")
+  @RequestMapping("/usr/home/getCount")
   @ResponseBody
-  public String showMain2() {
-    return "반갑습니다";
+  public int getCount() {
+     return count;
   }
-
-  @RequestMapping("/usr/home/main3")
+  @RequestMapping("/usr/home/doSetCount")
   @ResponseBody
-  public String showMain3() {
-    return "또 만나요";
-  }
-
-  @RequestMapping("/usr/home/main4")
-  @ResponseBody
-  public int showMain4() {
-
-    return ++count;
-  }
-  @RequestMapping("/usr/home/main5")
-  @ResponseBody
-  public String showMain5() {
-    count =0;
-    return "count 값이 0으로 초기화 되었습니다";
+  public String doSetCount(@RequestParam("count") int count) {
+    //3.2 스프링 부트부터 파라미터 이름 적용해줘야 함. ITJ는 설정에 저거 그냥 해주는 옵션이 있어서 일단 둘다 해놓음
+    this.count=0;
+    return "count 값이 " + this.count + "으로 초기화 되었습니다";
   }
 }
